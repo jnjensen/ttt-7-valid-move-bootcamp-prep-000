f96000da84849df423fa8bce19ab1a74fb21f66a
James Jensen <jnjensen@live.com>
2020-03-26 02:21:03 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,5 +2,19 @@
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+
 def position_taken?(array, token_index)
-  if 
+  x = array[token_index]
+  if x == " " || x == "" || x == nil
+    return false
+  elsif x == "X" || x == "O"
+    return true
+  end
+end
+
+def valid_move?(user_input)
+  y = user_input
+  if y >= 0 && y <= 8
+    return true
+  end
+end
